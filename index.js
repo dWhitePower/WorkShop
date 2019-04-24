4e36ab72503313a3def844aa8c4c007c64b848e4
dWhitePower <morgun055@gmail.com>
2019-04-24 13:59:50 +0300

    Added php

--- index.js
+++ index.js
@@ -67,6 +67,24 @@ $(window).on("scroll", function() {
   }
 });
 
+//E-mail Ajax Send
+$("form-member").submit(function() {
+  //Change
+  var th = $(this);
+  $.ajax({
+    type: "POST",
+    url: "mail.php", //Change
+    data: th.serialize()
+  }).done(function() {
+    alert("Thank you!");
+    setTimeout(function() {
+      // Done Functions
+      th.trigger("reset");
+    }, 1000);
+  });
+  return false;
+});
+
 // ScrollMagic
 
 const controller = new ScrollMagic.Controller();
